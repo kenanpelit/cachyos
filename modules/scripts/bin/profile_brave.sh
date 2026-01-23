9c8559098d0d7220617703ba52214fe62a22d125
kenanpelit <kenanpelit@gmail.com>
2026-01-23 23:21:28 +0300

    fix(brave): use basic password store to avoid keyring prompts

--- modules/scripts/bin/profile_brave.sh
+++ modules/scripts/bin/profile_brave.sh
@@ -60,6 +60,7 @@ readonly LOG_FILE="${HOME}/.config/brave-launcher/brave-launcher.log"
 		"--restore-last-session"
 		"--enable-features=TouchpadOverscrollHistoryNavigation,UseOzonePlatform,VaapiVideoDecoder"
 		"--ozone-platform=wayland"
+		"--password-store=basic"
 	)
 
 # Proxy ayarları
