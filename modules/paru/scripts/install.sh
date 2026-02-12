12e38abe72626f2f2996eefa61a5943488bb2f0a
Kenan Pelit <kenanpelit@gmail.com>
2026-02-12 09:47:04 +0300

    fix(paru): use absolute wrapper path and install paru.conf
    also: switch sesh module package to sesh-bin

--- modules/paru/scripts/install.sh
+++ modules/paru/scripts/install.sh
@@ -12,12 +12,26 @@ fi
 
 BIN_DIR="${USER_HOME}/.local/bin"
 WRAPPER_DST="${BIN_DIR}/pacman-sudo-paru"
+PARU_DIR="${USER_HOME}/.config/paru"
+PARU_CONF="${PARU_DIR}/paru.conf"
+PARU_CONF_CONTENT="# See paru.conf(5)
+[bin]
+Sudo = /usr/bin/sudo
+Pacman = ${WRAPPER_DST}
+"
 
 if [ "$(id -u)" -eq 0 ]; then
   USER_GROUP="$(id -gn "$REAL_USER" 2>/dev/null || true)"
   install -d -m 755 -o "$REAL_USER" -g "${USER_GROUP:-$REAL_USER}" "$BIN_DIR"
+  install -d -m 755 -o "$REAL_USER" -g "${USER_GROUP:-$REAL_USER}" "$PARU_DIR"
   install -m 755 -o "$REAL_USER" -g "${USER_GROUP:-$REAL_USER}" "$WRAPPER_SRC" "$WRAPPER_DST"
+  printf "%s" "$PARU_CONF_CONTENT" >"$PARU_CONF"
+  chown "$REAL_USER:${USER_GROUP:-$REAL_USER}" "$PARU_CONF"
+  chmod 644 "$PARU_CONF"
 else
   install -d -m 755 "$BIN_DIR"
+  install -d -m 755 "$PARU_DIR"
   install -m 755 "$WRAPPER_SRC" "$WRAPPER_DST"
+  printf "%s" "$PARU_CONF_CONTENT" >"$PARU_CONF"
+  chmod 644 "$PARU_CONF"
 fi
