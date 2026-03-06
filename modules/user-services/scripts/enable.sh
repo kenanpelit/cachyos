3207b7d492213bb13055ac8b09def9ce3485d8c6
Kenan Pelit <kenanpelit@gmail.com>
2026-03-06 17:10:02 +0300

    perf(noctalia): revert to clean systemd service with zero dependencies for speed and management

--- modules/user-services/scripts/enable.sh
+++ modules/user-services/scripts/enable.sh
@@ -33,6 +33,7 @@ services=(
   hyprland-bt-autoconnect.timer
   niri-bt-autoconnect.timer
   kdeconnect.timer
+  noctalia.service
   niri-bootstrap.service
   niri-sticky.service
   niri-niriswitcher.service
