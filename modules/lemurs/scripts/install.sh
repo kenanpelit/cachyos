16e039eae810b5a11d7b46ee9aa9460f5b69965e
Kenan Pelit <kenanpelit@gmail.com>
2026-04-22 11:54:44 +0300

    lemurs: own pam policy and sync wallpaper state

--- modules/lemurs/scripts/install.sh
+++ modules/lemurs/scripts/install.sh
@@ -9,6 +9,8 @@ VARIABLES_SRC="${SCRIPT_DIR}/../dotfiles/lemurs/variables.toml"
 VARIABLES_DST="/etc/lemurs/variables.toml"
 XSETUP_SRC="${SCRIPT_DIR}/../dotfiles/lemurs/xsetup.sh"
 XSETUP_DST="/etc/lemurs/xsetup.sh"
+PAM_SRC="${SCRIPT_DIR}/../dotfiles/pam.d/lemurs"
+PAM_DST="/etc/pam.d/lemurs"
 TTY_DROPIN_SRC="${SCRIPT_DIR}/../dotfiles/systemd/system/lemurs.service.d/10-tty7.conf"
 TTY_DROPIN_DST="/etc/systemd/system/lemurs.service.d/10-tty7.conf"
 KEYMAP_DROPIN_SRC="${SCRIPT_DIR}/../dotfiles/systemd/system/lemurs.service.d/10-vconsole-keymap.conf"
@@ -72,6 +74,7 @@ run_root install -d /etc/lemurs /etc/lemurs/wms /etc/lemurs/wayland
 install_root_file_if_changed "${CONFIG_SRC}" "${CONFIG_DST}" 644
 install_root_file_if_changed "${VARIABLES_SRC}" "${VARIABLES_DST}" 644
 install_root_file_if_changed "${XSETUP_SRC}" "${XSETUP_DST}" 755
+install_root_file_if_changed "${PAM_SRC}" "${PAM_DST}" 644
 install_root_file_if_changed "${TTY_DROPIN_SRC}" "${TTY_DROPIN_DST}" 644
 install_root_file_if_changed "${KEYMAP_DROPIN_SRC}" "${KEYMAP_DROPIN_DST}" 644
 
@@ -84,6 +87,7 @@ echo "Enabled: lemurs.service"
 echo "Installed: /etc/lemurs/config.toml"
 echo "Installed: /etc/lemurs/variables.toml"
 echo "Installed: /etc/lemurs/xsetup.sh"
+echo "Installed: /etc/pam.d/lemurs"
 echo "Installed: lemurs.service tty/keymap drop-ins"
 echo "Disabled (if present): gdm, greetd, sddm, lightdm, lxdm, lidm, ly, emptty"
 echo "Wayland session entries are provided by the sessions module."
