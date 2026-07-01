31b424e3e36cf84c12c818e2f689d928c90b0944
Kenan Pelit <kenanpelit@gmail.com>
2026-07-01 11:41:49 +0300

    update various

--- modules/search/scripts/install.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-if command -v systemctl >/dev/null 2>&1; then
-  systemctl --user daemon-reload >/dev/null 2>&1 || true
-  systemctl --user enable --now dsearch.service >/dev/null 2>&1 || true
-fi
